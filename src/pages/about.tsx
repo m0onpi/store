e5b1ba877847e489fce0da9c61a271b4c00a910e
m0onpi <michael.pigottbiz@gmail.com>
2023-07-16 16:28:34 +0100

    update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,11 +14,11 @@ function TermsPage() {
         </a>{" "}
       </p>
       <p>
-      Welcome to the Checkmate Boutique, your one-stop shop for all things chess. We understand the profound passion and dedication that players have for the royal game, and we've channelled that same devotion into curating an exceptional collection of chess-themed attire and accessories.
+      Welcome to the Checkmate Boutique, your one-stop shop for all things chess. We understand the profound passion and dedication that players have for the royal game, and we&apos;ve channelled that same devotion into curating an exceptional collection of chess-themed attire and accessories.
 
-In our store, you'll find a comprehensive range of fashion that allows you to display your love for chess in your everyday life. From chic T-shirts featuring intricate designs of your favorite openings, to hoodies that showcase famous positions from historic games, our attire merges style and your chess enthusiasm flawlessly.
+In our store, you&apos;ll find a comprehensive range of fashion that allows you to display your love for chess in your everyday life. From chic T-shirts featuring intricate designs of your favorite openings, to hoodies that showcase famous positions from historic games, our attire merges style and your chess enthusiasm flawlessly.
 
-But it doesn't end with clothing. Our selection of accessories includes everything you might imagine. Enrich your game experience with our artisanal chess boards and finely crafted pieces, or carry your chess pride with our themed bags, jewelry, and more. We even offer stylish chess clocks and score notebooks to support your tournament life!
+But it doesn&apos;t end with clothing. Our selection of accessories includes everything you might imagine. Enrich your game experience with our artisanal chess boards and finely crafted pieces, or carry your chess pride with our themed bags, jewelry, and more. We even offer stylish chess clocks and score notebooks to support your tournament life!
 
 At Checkmate Boutique, quality is paramount. Every item in our collection is designed with attention to detail and made from the best materials, ensuring you receive only the finest products. Whether you are a grandmaster, a club player, a chess enthusiast, or looking for a gift for one, Checkmate Boutique is the chess store that caters to all your chess-related needs.
 
