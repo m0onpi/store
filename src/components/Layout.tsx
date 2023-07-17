3ab5773e68244355f9cc7358d667bcb9a2ab959e
m0onpi <michael.pigottbiz@gmail.com>
2023-07-17 07:46:31 +0100

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,6 +11,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+    <div>
       <header className="py-6 md:py-12">
         <div className="max-w-6xl mx-auto px-6">
           <div className="flex items-center justify-between">
@@ -98,6 +99,7 @@ const Layout = ({ children }) => {
                   <path d="M4 16V4H2V2h3a1 1 0 0 1 1 1v12h12.438l2-8H8V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5a1 1 0 0 1-1-1zm2 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm12 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
                 </svg>
               </button>
+              
             </div>
           </div>
         </div>
@@ -143,6 +145,7 @@ const Layout = ({ children }) => {
           </nav>
         </div>
       </footer>
+      </div>
     </>
   );
 };
