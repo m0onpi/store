c24e4893f1b511e7d570e1ea221e2ba54965abaf
Michael <117381821+m0onpi@users.noreply.github.com>
2025-09-18 10:44:44 +0100

    add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
           />
           <script src="https://aifurniture.app/api/script?domain=chesspoop.fyi" async></script>
           <script src="https://aifurniture.app/api/order-tracker?domain=chesspoop.fyi" async></script>
+          <script src="https://aifurniture.app/api/tracking/script?domain=chesspoop.fyi" async></script>
         </Head>
 
         <body className="antialiased">
