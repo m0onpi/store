58d8eb02656128a1ae1b3d7f1893bcff93afa5fa
Michael <117381821+m0onpi@users.noreply.github.com>
2026-01-30 14:43:41 +0000

    add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
           <script src="https://aifurniture.app/api/script?domain=chesspoop.fyi" async></script>
           <script src="https://aifurniture.app/api/order-tracker?domain=chesspoop.fyi" async></script>
           <script src="https://aifurniture.app/api/tracking/script?domain=chesspoop.fyi" async></script>
-          <script src="https://aifurniture.app/api/chatbot-script?domain={{ shop.domain }}" async></script>
+          <script src="https://aifurniture.app/api/chatbot-script?domain=chesspoop.fyi" async></script>
         </Head>
 
         <body className="antialiased">
